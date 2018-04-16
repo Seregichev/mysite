c7c8014f7b8756d7dd5691c9bff92c0e59007264
victor <seregi4ev@gmail.com>
2018-04-17 00:27:40 +0300

    Разработана функция подбора модулей управления по парно согласно запрашиваемым параметрам (ПЛК и интелект. реле)

--- calculation/cms_plugins.py
+++ calculation/cms_plugins.py
@@ -11,6 +11,7 @@ from decimal import Decimal
 from .forms import CalcForm, CalcDriveForm, CalcNmb, CalcControlForm
 from .formula import add_commute_drive_items_into_estimate
 
+
 # Плагин настройки отображения плагина визализации подбора системы управления
 @python_2_unicode_compatible
 class CalcControlPluginSetting(CMSPlugin):
@@ -291,6 +292,9 @@ class CalcFormPluginSetting(CMSPlugin):
     def __str__(self):
         return self.get_title()
 
+# TODO: добавить при переносе функции добавления доп изделий управления
+from django.db.models.expressions import RawSQL
+from django.contrib import messages
 
 # Плагин добавления самой формы расчета и отправки в смету
 @plugin_pool.register_plugin
@@ -319,8 +323,202 @@ class CalcFormPlugin(CMSPluginBase):
                 add_commute_drive_items_into_estimate(request=request)
 
             if request.POST.get('calc_control', None):
+                print("  ")
                 print('Yes, the request from calc_control plugin is arrived')
-                # TODO: Написать функцию подбора изделий управления (лучше конечно в отдельном файле, но плодить код не хотелось бы)
+                choised_discret_input = request.POST.get('calc_control_discret_input') or 0
+                choised_discret_output = request.POST.get('calc_control_discret_output') or 0
+
+                def select_control_items(request, items_list, one_var_name, one_var_value, compatibility_code,
+                                         category__name, two_var_name="", two_var_value=0.0, places_list=None):
+                    # items_list[][] - двумерный список отобранных изделий, в котором items_list[модель изделия][кол-во]
+
+                    # получаем queryset изделий из запрашиваемой категории по коду совместимости
+                    modules = Item.objects.filter(category__name=category__name,
+                                                  compatibility_code=compatibility_code)
+
+                    # сторожевая собака чтобы не зависнуть в цикле
+                    watchdog = 0
+
+                    # Переменная наличия ошибки при подборе
+                    error = False
+
+                    # открываем цикл, пока не подберем нужное колличество изделий с заданным кол-вом параметров
+                    while one_var_value > 0 or two_var_value > 0 and watchdog < 1000:
+                        watchdog +=1
+
+                        # создаем первую выборку изделий
+                        first_items = modules
+
+                        # отсортировываем выборку на наличие запрашиваемых параметров
+                        if one_var_value > 0:
+                            first_items = first_items.filter(variables__contains=one_var_name)
+
+                        if two_var_value > 0:
+                            first_items = first_items.filter(variables__contains=two_var_name)
+
+                        # переносим первую выборку во вторую
+                        second_items = first_items
+
+                        # фильтруем вторую выборку на условии меньше или равно искомому колличеству
+                        # сортируем выборку по искомому параметру в порядке возрастания
+                        if one_var_value > 0:
+                            second_items = second_items.filter(variables__lte={one_var_name: one_var_value})
+                            # способ сортировки по hstore полю в порядке возрастания
+                            second_items = second_items.order_by(
+                                RawSQL("Cast(variables->%s as Integer)", (one_var_name,)))
+
+                        if two_var_value > 0:
+                            second_items = second_items.filter(variables__lte={two_var_name: two_var_value})
+                            second_items = second_items.order_by(
+                                RawSQL("Cast(variables->%s as Integer)", (two_var_name,)))
+
+                        # переносим вторую выборку в третью
+                        third_items = second_items
+
+                        # TODO: проверить работу or not third_items.exists() и при не нужности перенести для норм работы
+                        # исключаем из выборки если кол-во запрашиваемых параметров подобрано или черезмерно подобрано
+                        if one_var_value <= 0:
+                            third_items = third_items.exclude(variables__contains=one_var_name)
+                        if two_var_value <= 0:
+                            third_items = third_items.exclude(variables__contains=two_var_name)
+
+                        # запрашиваем на наличие изделий в выборке - если нет, то возвращаемся ко второй выборке изделий
+                        if not third_items.exists():
+                            third_items = second_items
+
+                        # переносим выборку в итоговые изделия
+                        items = third_items
+
+                        # если изделия есть в выбоке то делаем порядковый реверс что бы подобранное изделий с максимально возможным параметром было первым в выдаче
+                        if items:
+                            items = items.reverse()
+
+                        # если изделий нетб то возвращаемся к первой выборке и сортируем ее в порядке возрастания запрашиваемого параметра
+                        else:
+                            items = first_items
+                            if one_var_value > 0:
+                                items = items.order_by(RawSQL("Cast(variables->%s as Integer)", (one_var_name,)))
+                            if two_var_value > 0:
+                                items = items.order_by(RawSQL("Cast(variables->%s as Integer)", (two_var_name,)))
+
+                        # Если и в этом случае нет изделий, прерываем цикл и выдаем сообщение об ошибке
+                        if not items:
+                            messages.info(request, u'Ошибка: невозможно выполнить подбор. Уменьшите заданные критерии.')
+                            error = True
+                            break
+
+                        # получаем первое идущее в выборке изделие
+                        item = items.first()
+
+                        # если изделие присутствует и список подобранных изделий содержит первую позицию
+                        if item and len(items_list):
+
+                            # обнуляем счетчик цикла for
+                            for_index = 0
+
+                            # обходим циклом все доступные места и их названия для доп модулей
+                            for place_name, place_value in places_list:
+                                place_value = int(place_value)
+
+                                # пытаемся получить Это место из изделия
+                                try:
+                                    if item.variables[place_name]:
+
+                                        # если кол-во доступных мест установки больше 0, то
+                                        if place_value > 0:
+                                            # если в списке подобранных изделий последнее изделие не равно текущему подобранному изделию, то добавляем текущее подобранное изделие в список подобранных изделий
+                                            if items_list[len(items_list) - 1][0] != item:
+                                                items_list.append([item, 1])
+
+                                            # иначе если текущее подобранное изделие есть в списке подобранных изделий то увеличиваем кол-во последнего изделия в списке подобранных изделий
+                                            elif items_list[len(items_list) - 1][0] == item:
+                                                items_list[len(items_list) - 1][1] += 1
+
+                                            # пытаемся вычесть кол-во подобранных параметров из выбранного изделия
+                                            if one_var_value > 0:
+                                                try:
+                                                    one_var_value -= int(item.variables[one_var_name])
+                                                except (AttributeError, KeyError):
+                                                    one_var_value = 0
+                                                    messages.info(request,
+                                                                  u'Ошибка: не возможно вычесть %s при подборе управления' % one_var_name)
+                                                    error = True
+
+                                            if two_var_value > 0:
+                                                try:
+                                                    two_var_value -= int(item.variables[two_var_name])
+                                                except (AttributeError, KeyError):
+                                                    two_var_value = 0
+                                                    messages.info(request,
+                                                                  u'Ошибка: не возможно вычесть %s при подборе управления' % two_var_name)
+                                                    error = True
+
+                                            # вычитаем кол-во Этих мест с изделия которое добавили
+                                            place_value -= int(item.variables[place_name])
+                                            # перезаписываем название и кол-во мест в списке мест
+                                            places_list[for_index]=[place_name, place_value]
+
+                                        # если кол-во доступных мест в изделии меньше или = 0, то исключаем из выборки module (перед циклом while) все изделия (модули) с таким требуемым расположением
+                                        else:
+                                            modules = modules.exclude(variables__contains=place_name)
+                                            try:
+                                                if items_list[len(items_list) - 1][0].variables[place_name]:
+                                                    items_list.pop()
+                                            except KeyError: None
+
+                                # если при получении переменной hstore с именем запрашиваемого места расположения модуля происходит ошибка ключа (ключ не найден) то ничего не делаем и пропускаем выборку
+                                except KeyError: None
+
+                                for_index +=1
+
+                    return request, items_list, one_var_value, two_var_value, error
+
+                discret_input=round(int(choised_discret_input),0)
+                discret_output = round(int(choised_discret_output),0)
+
+                if settings.DEBUG:
+                    messages.info(request, u'Запрашиваемые дискретные входа: %s, и выхода: %s ' % (discret_input, discret_output))
+
+                items_list=[]
+                index=0
+                cpu = Item.objects.get(id=882)
+                print('This is CPU:')
+                places_list = []
+                try: places_list.append(["left_places", cpu.variables["left_places"]])
+                except KeyError: None
+                try: places_list.append(["right_places", cpu.variables["right_places"]])
+                except KeyError: None
+                try: places_list.append(["board_places", cpu.variables["board_places"]])
+                except KeyError: None
+
+                print(places_list)
+                for key, value in places_list:
+                    print("key: %s, value: %s" % (key, value))
+
+                print(cpu.variables["left_places"])
+                print(' ')
+                items_list.append([cpu, 1])
+
+                request, items_list, discret_input, discret_output, \
+                                                 error = select_control_items(request=request, items_list=items_list,
+                                                                              one_var_name="discret_input",
+                                                                              one_var_value=discret_input,
+                                                                              compatibility_code=cpu.compatibility_code,
+                                                                              category__name=u"Модуль расширения ПЛК",
+                                                                              two_var_name="discret_output",
+                                                                              two_var_value=discret_output,
+                                                                              places_list=places_list
+                                                                              )
+
+                messages.info(request,
+                              u'Осталось первого параметра %s, второго параметра %s' % (discret_input, discret_output))
+
+                # TODO: удалить ниже следующее после пусконаладки
+                print(items_list)
+                print("  ")
+                if settings.DEBUG:
+                    for key, value in items_list:
+                        messages.info(request, u'Изделие: %s, кол-во: %s ' % (key.name, value))
 
             if not request.POST._mutable:
                 request.POST._mutable = True
@@ -329,3 +527,5 @@ class CalcFormPlugin(CMSPluginBase):
 
         context = super(CalcFormPlugin, self).render(context, instance, placeholder)
         return context
+
+# TODO: Перенести функцию добавления модулей управления в отдельный файл
